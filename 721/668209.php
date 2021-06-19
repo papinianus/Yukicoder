e169cdd133cf32d53e859480776dfbccbfbaa92b
papinianus <dglmsksooah@gmail.com>
2021-06-19 14:43:56 +0900

    +1

---
+++ 721/668209.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+$d = new DateTimeImmutable(UtilIO::getString());
+$dayAfterTomorrow = $d->add(new DateInterval('P2D'));
+UtilIO::echo($dayAfterTomorrow->format('Y/m/d'));
+
+
+class UtilIO {
+    protected static string $n = PHP_EOL;
+    protected function __construct() { }
+    public static function getString():string {
+        return trim(fgets(STDIN));
+    }
+    public static function getInt():int {
+        return intval(trim(fgets(STDIN)));
+    }
+    public static function echo(string $str):void {
+        echo static::toLine($str);
+    }
+    public static function toLine(string $str):string {
+        return trim($str).static::$n;
+    }
+}
