c3af0dcb9abcb3941b652c8017be29eb5c88326f
papinianus <dglmsksooah@gmail.com>
2022-08-07 10:46:21 +0900

    1 x level 1

---
+++ 2014/785930.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+UtilIO::echo(strval(min(UtilIO::getStringArray())));
+
+class UtilIO {
+    protected static string $n = PHP_EOL;
+    protected function __construct() { }
+    public static function getString():string {
+        return trim(fgets(STDIN));
+    }
+    public static function getInt():int {
+        return intval(static::getString());
+    }
+    public static function getStringArray($separator = " "):array {
+        return explode($separator, static::getString());
+    }
+    public static function getIntArray($separator = " "):array {
+        return array_map('intval', static::getStringArray($separator));
+    }
+    public static function echo(string $str):void {
+        echo static::toLine($str);
+    }
+    public static function toLine(string $str):string {
+        return trim($str).static::$n;
+    }
+}
