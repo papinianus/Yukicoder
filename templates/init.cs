0c9817064b5d744bd1ced1fa3c156d34a86dbbb4
papinianus <dglmsksooah@gmail.com>
2022-08-07 10:15:34 +0900

    create template from old posts

---
+++ templates/init.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+namespace Yukicoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    static class StringExtention
+    {
+        public static string[] ToStringArray(this string value, char splitChar) => value.Split(splitChar);
+        public static int[] ToIntArray(this string value, char splitChar) => value.Split(splitChar).Select(x=>Convert.ToInt32(x)).ToArray();
+        public static long[] ToLongArray(this string value, char splitChar) => value.Split(splitChar).Select(x => Convert.ToInt64(x)).ToArray();
+    }
+}
